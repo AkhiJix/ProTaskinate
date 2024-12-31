25a5a9ce7a5392009570c256d3acccb70dcdb9f3
Akhilesh <jichkarakhilesh@gmail.com>
2024-12-31 13:11:21 +0530

    working app completed

--- app/src/main/java/com/akhijix/protaskinate/ui/deleteallcompleted/DeleteAllCompletedViewModel.kt
+++ app/src/main/java/com/akhijix/protaskinate/ui/deleteallcompleted/DeleteAllCompletedViewModel.kt
@@ -4,16 +4,16 @@ import androidx.lifecycle.ViewModel
 import com.akhijix.protaskinate.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DeleteAllCompletedViewModel @Inject constructor(
     private val taskDao : TaskDao,
+    private val applicationScope: CoroutineScope
 ) : ViewModel() {
 
-    fun onConfirmClicked() = MainScope().launch {
+    fun onConfirmClicked() = applicationScope.launch {
         taskDao.deleteCompletedTasks()
     }
 }
