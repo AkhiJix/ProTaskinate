32971864e39a1398dfb107a3b5cf8b0f74d9c580
Akhilesh <jichkarakhilesh@gmail.com>
2025-01-01 19:38:35 +0530

    pushing to git

---
+++ app/src/main/java/com/akhijix/protaskinate/data/TaskDatabase.kt
@@ -0,0 +1,39 @@
+package com.akhijix.protaskinate.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class TaskDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    class Callback @Inject constructor(
+        private val database: Provider<TaskDatabase>,
+        private val applicationScope : CoroutineScope
+    ) : RoomDatabase.Callback(){
+
+        override fun onCreate(db: SupportSQLiteDatabase){
+            super.onCreate(db)
+
+            val dao = database.get().taskDao()
+
+            applicationScope.launch {
+                dao.insertTask(Task("Buy Groceries",isImportant = true))
+                dao.insertTask(Task("Wash the Dishes"))
+                dao.insertTask(Task("Do the Laundry"))
+                dao.insertTask(Task("PrepareFood", isCompleted = true))
+                dao.insertTask(Task("Call mom"))
+                dao.insertTask(Task("Visit grandma", isCompleted = true))
+                dao.insertTask(Task("Repair my Bike"))
+                dao.insertTask(Task("Call Ambani"))
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/akhijix/protaskinate/ui/tasks/TasksViewModel.kt
@@ -0,0 +1,72 @@
+package com.akhijix.protaskinate.ui.tasks
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.akhijix.protaskinate.data.PreferencesManager
+import com.akhijix.protaskinate.data.SortOrder
+import com.akhijix.protaskinate.data.Task
+import com.akhijix.protaskinate.data.TaskDao
+import dagger.assisted.Assisted
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TasksViewModel @Inject constructor(
+    private val taskDao: TaskDao,
+    private val preferencesManager: PreferencesManager
+) : ViewModel() {
+
+    val searchQuery = MutableStateFlow("")
+
+    val preferencesFlow = preferencesManager.preferencesFlow
+
+    private val tasksEventChannel = Channel<TasksEvent>()
+    val tasksEvent = tasksEventChannel.receiveAsFlow()
+
+    private val tasksFlow = combine(
+        searchQuery,
+        preferencesFlow
+    ) { query, filterPreferences ->
+        Pair(query, filterPreferences)
+    }.flatMapLatest { (query, filterPreferences) ->
+        taskDao.getTasks(query, filterPreferences.sortOrder, filterPreferences.hideCompleted)
+    }
+
+    val tasks = tasksFlow.asLiveData()
+
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onHideCompletedClick(hideCompleted: Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
+    }
+
+    fun onTaskSelected(task: Task) {}
+
+    fun onTaskCheckedChanged(task: Task, isChecked: Boolean) = viewModelScope.launch {
+        taskDao.updateTask(task.copy(isCompleted = isChecked))
+    }
+
+    fun onTaskSwiped(task: Task) = viewModelScope.launch {
+        taskDao.deleteTask(task)
+        tasksEventChannel.send(TasksEvent.ShowUndoDeleteTaskMessage(task))
+    }
+
+    fun onUndoDeleteClick(task: Task) = viewModelScope.launch {
+        taskDao.insertTask(task)
+    }
+
+    sealed class TasksEvent {
+        data class ShowUndoDeleteTaskMessage(val task: Task) : TasksEvent()
+    }
+}
