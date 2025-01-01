ec9a87b51cd52f11848ff534b9c528978d5af061
Akhilesh <jichkarakhilesh@gmail.com>
2025-01-01 19:33:02 +0530

    minor changes in db

--- app/src/main/java/com/akhijix/protaskinate/data/TaskDatabase.kt
+++ app/src/main/java/com/akhijix/protaskinate/data/TaskDatabase.kt
@@ -8,23 +8,24 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Provider
 
-@Database(entities = [Task::class], version = 1, exportSchema = false)
+@Database(entities = [Task::class], version = 1)
 abstract class TaskDatabase : RoomDatabase() {
 
     abstract fun taskDao(): TaskDao
+    // use shortcut dbcallback for a sample callback class
 
     class Callback @Inject constructor(
         private val database: Provider<TaskDatabase>,
-        private val applicationScope : CoroutineScope
-    ) : RoomDatabase.Callback(){
+        private val applicationScope: CoroutineScope
+    ) : RoomDatabase.Callback() {
 
-        override fun onCreate(db: SupportSQLiteDatabase){
+        override fun onCreate(db: SupportSQLiteDatabase) {
             super.onCreate(db)
 
             val dao = database.get().taskDao()
 
             applicationScope.launch {
-                dao.insertTask(Task("Buy Groceries",isImportant = true))
+                dao.insertTask(Task("Buy Groceries", isImportant = true))
                 dao.insertTask(Task("Wash the Dishes"))
                 dao.insertTask(Task("Do the Laundry"))
                 dao.insertTask(Task("PrepareFood", isCompleted = true))
@@ -35,5 +36,4 @@ abstract class TaskDatabase : RoomDatabase() {
             }
         }
     }
-
 }
